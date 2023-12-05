9716f9c4ac76dee522cb73ac9080fabcccc6425c
beau-ottens <bo@ehvg.nl>
2023-12-05 16:27:12 +0100

    [DV-6723] Add author

--- src/Exception/ServerException.php
+++ src/Exception/ServerException.php
@@ -11,6 +11,7 @@ use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
  * Thrown when a 5xx response is returned.
  *
  * @author Niels Nijens <nn@superbrave.nl>
+ * @author Beau Ottens <bo@ehvg.nl>
  */
 final class ServerException extends RuntimeException implements ServerExceptionInterface
 {
